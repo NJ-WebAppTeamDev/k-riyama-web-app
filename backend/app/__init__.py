d6fbe27e36f5d3e720a3418b7e84f006088f30ce
KadoProG <splatrain@gmail.com>
2024-08-03 05:34:18 +0900

    update format

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,7 +7,12 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://localhost:8080"]}})
+    CORS(
+        app,
+        resources={
+            r"/*": {"origins": ["http://localhost:3000", "http://localhost:8080"]}
+        },
+    )
     app.config.from_object("config.Config")
     db.init_app(app)
 
