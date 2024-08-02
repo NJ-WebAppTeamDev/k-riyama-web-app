f8829a2f3ddfb5e00e88edf6e27d1bdc833e1767
nakagawahina <hina.17.hina@icloud.com>
2024-08-02 10:40:15 +0900

    #7

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import style from "./Footer.module.css";
+
+function Footer() {
+  return (
+  <footer>
+    <div className={style.test}>
+      <p className={style.test}>&copy; 2024 Joken</p>
+    </div>
+  </footer>
+  );
+}
+
+export default Footer;
