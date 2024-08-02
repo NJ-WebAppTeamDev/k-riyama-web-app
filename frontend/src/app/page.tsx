0ddbbf23c660f1a80384c8640a96c1565133dcaf
takutou <takuto1488@gmail.com>
2024-08-02 11:58:50 +0900

    #12 ホーム画面のcss適用

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,23 +8,43 @@ function Home() {
       <div className={style.emptybox}></div>
       <div className={style.tutorial}>
         <h2>アプリの使い方</h2>
-        <div className={style.tutorial_item}>
-          <Image src="/mock/100x100.png" alt="test" height={100} width={100} />
-          <p className={style.tutorial_description}>
-            地図上のQRコードを読み取ってパネルをゲット
-          </p>
-        </div>
-        <div className={style.tutorial_item}>
-          <Image src="/mock/100x100.png" alt="test" height={100} width={100} />
-          <p className={style.tutorial_description}>
-            合計9枚のパネルを集めてスタンプボードの絵を完成させよう
-          </p>
-        </div>
-        <div className={style.tutorial_item}>
-          <Image src="/mock/100x100.png" alt="test" height={100} width={100} />
-          <p className={style.tutorial_description}>
-            完成した絵をスタッフに見せて景品ゲット
-          </p>
+        <div className={style.tutorial_item__wrapper}>
+          <div className={style.tutorial_item}>
+            <Image
+              className={style.tutorial_image}
+              src="/mock/100x100.png"
+              alt="test"
+              height={100}
+              width={100}
+            />
+            <p className={style.tutorial_description}>
+              地図上のQRコードを読み取ってパネルをゲット
+            </p>
+          </div>
+          <div className={style.tutorial_item}>
+            <Image
+              className={style.tutorial_image}
+              src="/mock/100x100.png"
+              alt="test"
+              height={100}
+              width={100}
+            />
+            <p className={style.tutorial_description}>
+              合計9枚のパネルを集めてスタンプボードの絵を完成させよう
+            </p>
+          </div>
+          <div className={style.tutorial_item}>
+            <Image
+              className={style.tutorial_image}
+              src="/mock/100x100.png"
+              alt="test"
+              height={100}
+              width={100}
+            />
+            <p className={style.tutorial_description}>
+              完成した絵をスタッフに見せて景品ゲット
+            </p>
+          </div>
         </div>
       </div>
       <div className={style.stump}>
