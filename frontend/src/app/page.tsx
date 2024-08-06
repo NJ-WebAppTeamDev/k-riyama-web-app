150646efcffc7783b0a42c66558a2868d47f5edc
nakagawahina <hina.17.hina@icloud.com>
2024-08-06 17:24:28 +0900

    #7  ハンバーガーメニュー完成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,24 +1,26 @@
 "use client";
 import styles from '../components/buger.module.css';
+import Image from 'next/image';
+import logo from "../app/images/icon.jpg";
 
 export default function Home() {
   return (
-      <header>
-        <div className={styles.hamburgerMenu}>
-          <input type="checkbox" id="menu-btn-check" className={styles.menuBtnCheck} />
-          <label htmlFor="menu-btn-check" className={styles.menuBtn}><span></span></label>
-          <div className={styles.menuContent}>
-            <ul>
-              <li><a href="#">ホーム</a></li>
-              <li><a href="#">使い方</a></li>
-              <li><a href="#">お問い合わせ</a></li>
-              <li><a href='#'>ライセンス</a></li>
-            </ul>
-          </div>
+    <header className={styles.header}>
+      <div className="App">
+        <Image src={logo} alt="Logo" width={90} height={90} />
+      </div>
+      <div className={styles.hamburgerMenu}>
+        <input type="checkbox" id="menu-btn-check" className={styles.menuBtnCheck} />
+        <label htmlFor="menu-btn-check" className={styles.menuBtn}><span></span></label>
+        <div className={styles.menuContent}>
+          <ul>
+            <li><a href="#">ホーム</a></li>
+            <li><a href="#">使い方</a></li>
+            <li><a href="#">お問い合わせ</a></li>
+            <li><a href="#">ライセンス</a></li>
+          </ul>
         </div>
-        <div className = "App">
-          <img src="./frontend/src/app/images/icon.jpg" className="App-logo" alt="logo"/>
-        </div>
-      </header>
-    );
-  } 
+      </div>
+    </header>
+  );
+}
