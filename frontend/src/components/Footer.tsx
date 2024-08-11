2220e41b3c6d23a00cff2cb5c31ebfc7ae4d7e6f
KadoProG <splatrain@gmail.com>
2024-08-11 14:34:01 +0900

    add prettier and format

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,11 +2,11 @@ import style from "./Footer.module.css";
 
 function Footer() {
   return (
-  <footer>
-    <div className={style.test}>
-      <p className={style.character}>&copy; 2024 Joken</p>
-    </div>
-  </footer>
+    <footer>
+      <div className={style.test}>
+        <p className={style.character}>&copy; 2024 Joken</p>
+      </div>
+    </footer>
   );
 }
 export default Footer;
