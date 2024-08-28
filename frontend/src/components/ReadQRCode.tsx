850983f96abc460cefa341943eb532982677b030
tacotaco0808 <takuto1488@gmail.com>
2024-08-28 18:25:30 +0900

    #42 リストに指定したURLのデータのみ保存

--- frontend/src/components/ReadQRCode.tsx
+++ frontend/src/components/ReadQRCode.tsx
@@ -1,15 +1,41 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useZxing } from 'react-zxing';
 
 function ReadQRCode() {
   const [result, setResult] = useState('');
+  const [stampsList, setStampsList] = useState<string[]>([]);
   const { ref } = useZxing({
     onDecodeResult(result) {
       setResult(result.getText());
+      console.log(result.getText());
+      if (
+        result.getText() === 'https://x.com/home?lang=ja' ||
+        result.getText() === 'https://www.instagram.com/' ||
+        result.getText() === 'https://www.youtube.com/'
+      ) {
+        addStamp(result.getText());
+      }
     },
   });
-
+  /*ローカルストレージに保存されているスタンプを読み込み*/
+  useEffect(() => {
+    const storedStamps = localStorage.getItem('stamps');
+    if (storedStamps) {
+      setStampsList(JSON.parse(storedStamps));
+    }
+  }, []);
+  /*読み込んだスタンプをローカルストレージに保存 */
+  const addStamp = (newStamp: string) => {
+    if (stampsList.includes(newStamp)) {
+      alert('すでに読み込んだQRコードです');
+      return;
+    }
+    const updatedStampsList = [...stampsList, newStamp];
+    setStampsList(updatedStampsList);
+    localStorage.setItem('stamps', JSON.stringify(updatedStampsList));
+    alert('Add Stamp');
+  };
   return (
     <>
       <video ref={ref} />
